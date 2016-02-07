c009d89e36291db25dcf8b29b99bddaeae848056
Brian Stovall <hoppenmaus@gmail.com>
2016-02-07 17:40:52 -0500

    changed BG image. requests are now made and run asynchronously. styled infoDivs and fixed some control/focus issues

--- twitch.js
+++ twitch.js
@@ -6,47 +6,100 @@ document.addEventListener('DOMContentLoaded', function() {
 	var sortOnline = grab('sort-online');
 	var sortOffline = grab('sort-offline');
 	var addChannel = grab('add-channel');
-
 	var buttons = document.getElementsByClassName('btn');
 
-	//holders for dynamically generated elements
-	var onlineStreams;
-	var offlineStreams;
-	
-	var twitchRequest = new XMLHttpRequest();
 	var APIPrefix = 'https://api.twitch.tv/kraken/channels/';
 
+	//maintain a list of all channels displayed so far
+	var displayed = [];
+
+	//list of channels to autoload
+	var preloads = ["freecodecamp", "storbeck",  
+		"habathcx","RobotCaleb","noobs2ninjas"];
+
+	//preload
+	for (var i = 0; i < preloads.length; i++) 
+		makeRequest(preloads[i]);
+
+	//give focus to addChannel
+	addChannel.focus();
+
+	//helper fn - returns boolean based on if channel 
+	//is currently displayed on page
+	function isDisplayed(channel) {
+		return (displayed.indexOf(channel) !== -1);
+	}
+
+	//handle text requests
 	addChannel.onkeyup = function (event) {
 		if (event.key === 'Enter') {
-			twitchRequest.open('GET', APIPrefix + this.value);
+			if (this.value !== '' && !isDisplayed(this.value)) {
+				//check the api for the channel
+				makeRequest(this.value);
+			}
+			//add this attempt to displayed, even if it's bogus
+			displayed.push(this.value);
+			//no matter what, clear the box
+			this.value = ''; 
+		}
+	}
+
+	//loads and sends twitchRequest with channelName
+	function makeRequest(channelName) {
+		var twitchRequest = new XMLHttpRequest();
+
+		twitchRequest.onload = function() {
+			if (twitchRequest.status >= 200 && twitchRequest.status <= 400) {
+				console.log('success! rec\'d: ' + twitchRequest.responseText);
+				populate(JSON.parse(twitchRequest.responseText));
+			} else  {
+				//let the user know
+				alert('Channel wasn\'t found on Twitch.tv.');
+				console.log('fail?! rec\'d: ' + twitchRequest.responseText);
+			}
+		}
+	
+			twitchRequest.open('GET', APIPrefix + channelName);
 			twitchRequest.send();
 			console.log('request sent');
-		}
 	}
+
 			
-	twitchRequest.onload = function() {
-		if (twitchRequest.status >= 200 && twitchRequest.status <= 400) {
-			console.log('success! rec\'d: ' + twitchRequest.responseText);
-			populate(JSON.parse(twitchRequest.responseText));
-		} else  {
-			//let the user know
-			alert('Channel wasn\'t found on Twitch.tv.');
-			console.log('fail?! rec\'d: ' + twitchRequest.responseText);
-		}
-	}
 
 	//adds a successfully found channel to the page
 	function populate(data) {
 		console.log('contains keys: ' + Object.keys(data));
 		console.log('populating');
+
 		//a link to the channel to wrap the containing div in
 		var channelLink = document.createElement('a');
-
 		channelLink.href = data.url;
+
+		//the container for the data
 		var infoDiv = document.createElement('div');
 		infoDiv.classList.add('infoDiv');
-		infoDiv.textContent = data.name;
 		channelLink.appendChild(infoDiv);
+
+		//subcontainer for the user pic
+		var userPic = document.createElement('div');
+		userPic.classList.add('userPic');
+		userPic.style['background-image'] = (data.logo) ? 'url(' + data.logo + ')':
+				'url(\'assets/question.png\')';
+		infoDiv.appendChild(userPic);
+
+		//subcontainer for the channel name
+		var name = document.createElement('h3');
+		name.classList.add('userName');
+		name.textContent = data.name;
+		infoDiv.appendChild(name);
+
+		//subcontainer for the channel status
+		var userStatus = document.createElement('p');
+		userStatus.classList.add('userStatus');
+		userStatus.textContent = data.status;
+		infoDiv.appendChild(userStatus);
+
+		//add new elements to document
 		container.appendChild(channelLink);
 	}
 
